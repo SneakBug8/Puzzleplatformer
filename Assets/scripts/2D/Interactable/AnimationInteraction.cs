3f05c27df98af3d65d62d3773909d237f54b394a
SneakBug8 <sneakbug8@gmail.com>
2018-02-21 00:04:45 +0300

    Interactables

---
+++ Assets/scripts/2D/Interactable/AnimationInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationInteraction : Interactable {
+	Animator Animator;
+
+	public string Trigger;
+	// Use this for initialization
+	void Start () {
+		Animator = GetComponent<Animator>();
+	}
+	
+	public override void Interact() {
+		Animator.SetTrigger(Trigger);
+	}
+}
