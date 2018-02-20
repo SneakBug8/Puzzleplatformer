a8692a38511305d9d100b3938a480b1eff1bcc98
SneakBug8 <sneakbug8@gmail.com>
2018-02-20 15:47:15 +0300

    LogicalLaserGenerator and LogicalTrue

---
+++ Assets/scripts/Logic/LogicalLaserGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalLaserGenerator : LogicalEnd
+{
+    public bool AutoRelaunch;
+    protected GameObject Laser;
+    public bool ChangeState;
+	public bool Inverse;
+
+    public override void Process()
+    {
+        if (ChangeState && Element.Return)
+        {
+            Laser.SetActive(!Laser.activeSelf);
+        }
+        else
+    	{
+			bool active = (!Inverse && Element.Return) || (Inverse && !Element.Return);
+            if (!active)
+            {
+                Laser.SetActive(false);
+            }
+            else if (active && AutoRelaunch)
+            {
+                Laser.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/scripts/Logic/LogicalTrue.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class LogicalTrue : LogicalElement {
+    void Start() {
+        Return = true;
+    }
+}
