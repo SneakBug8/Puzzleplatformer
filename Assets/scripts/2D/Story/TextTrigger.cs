f624610c18437bdc6f75a24e3a89f4642b663f5a
SneakBug8 <sneakbug8@gmail.com>
2018-02-24 16:50:34 +0300

    Story Text Trigger

---
+++ Assets/scripts/2D/Story/TextTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Collider2D))]
+public class TextTrigger : MonoBehaviour {
+	public string TextString;
+	public StoryText Text;
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject == Player.Global.gameObject) {
+			Text.PrintText(TextString).SetFade(true).DisableAfterTime(5f).Enable();
+		}
+	}
+}
